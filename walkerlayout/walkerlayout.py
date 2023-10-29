607831bbb665ecff1640c8d63bc3191799c4166c
Elias Foramitti <mitti.leon@gmail.com>
2023-10-29 05:30:24 +0100

    setting up package distribution

--- walkerlayout/walkerlayout.py
+++ walkerlayout/walkerlayout.py
@@ -401,3 +401,5 @@ class Node(Generic[T]):
         left_subtree.change = left_subtree.change + shift / subtrees
         self.x = self.x + shift
         self.modifier = self.modifier + shift
+
+__all__ = ["WalkerLayouting"]
